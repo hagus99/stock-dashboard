9bdc5ed5f543a18f47cd0dc91d1114235467e631
문하현 <munsang-gil@Moon-MacBook.local>
2025-12-06 23:03:44 +0900

    fix: Remove base path from Vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/stock-dashboard/", // 예: '/stock-dashboard/' (앞뒤로 슬래시 필수)
 });
